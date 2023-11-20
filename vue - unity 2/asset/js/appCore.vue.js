a02588846c121c062393e1c0d9aa1f88e59de2e8
Agustin Salum <agustinsalum92@hotmail.com>
2023-11-20 17:24:09 -0300

    Detalle en agregar pedidos

--- vue - unity 2/asset/js/appCore.vue.js
+++ vue - unity 2/asset/js/appCore.vue.js
@@ -1,15 +1,36 @@
-const order = {
-    id: crypto.randomUUID(),
-    status: true,
-    date: new Date().toISOString().split("T")[0],
-    name: '',
-    amount:'',
-    price:'',
-}
 
 export const appCore = {
-    data: () => ({
-        message: 'Hello',
-        order: order,
-    }),
+    data: function () {
+        return {
+            message: 'Hello',
+            order: {
+                id: crypto.randomUUID(),
+                status: true,
+                date: new Date().toISOString().split('T')[0],
+                name: '',
+                amount: '',
+                price: '',
+            },
+            orderList: [],
+        };
+    },
+
+
+    methods: {
+        order_handler: function () {
+            console.log(this.order);
+            let orderNew = this.order;
+            this.orderList.push(orderNew);
+            this.order = {
+                id: crypto.randomUUID(),
+                status: true,
+                date: new Date().toISOString().split("T")[0],
+                name: '',
+                amount: '',
+                price: '',
+            };
+            /* console.log(this.orderList); */
+
+        }
+    }
 }
