18892ee351cac40291dd4973f55db42924b654f6
Agustin Salum <agustinsalum92@hotmail.com>
2023-11-26 16:10:47 -0300

    Agregado boton desactivar, api y componentes

--- vue - unity 2/asset/js/appCore.vue.js
+++ vue - unity 2/asset/js/appCore.vue.js
@@ -1,10 +1,26 @@
 
+/* components in vue */
+const allComponent = {
+    template:
+        `<p> id: {{id}}</p>
+         <p> Name: {{ title }}</p > 
+        `
+    ,
+    props: {
+        title: String,
+        id: Number,
+    }
+
+}
+
+
 export const appCore = {
     data: function () {
         return {
             message: 'Hello',
+            classInactive: "inactive",
             order: {
-                id: crypto.randomUUID(),
+                id: crypto.randomUUID().slice(0, 14),
                 status: true,
                 date: new Date().toISOString().split('T')[0],
                 name: '',
@@ -13,17 +29,29 @@ export const appCore = {
             },
             orderList: [],
             cant_order: null,
+            all: null,
         };
     },
 
+    components: {
+        all: allComponent,
+    }
+    ,
+
 
     methods: {
+        fetchHandler: function () {
+            fetch("https://jsonplaceholder.typicode.com/todos", {})
+                .then(res => res.json())
+                .then(data => this.all = data)
+                .catch(err => console.log({ err }))
+        },
         order_handler: function () {
             console.log(this.order);
             let orderNew = this.order;
             this.orderList.push(orderNew);
             this.order = {
-                id: crypto.randomUUID(),
+                id: crypto.randomUUID().slice(0, 14),
                 status: true,
                 date: new Date().toISOString().split("T")[0],
                 name: '',
@@ -33,6 +61,11 @@ export const appCore = {
             /* console.log(this.orderList); */
 
         },
+        deleteHandler: function (id) {
+            this.orderList = this.orderList.filter(
+                (order) => order.id != id
+            )
+        }
     },
 
     /* https://vuejs.org/guide/essentials/computed.html */
